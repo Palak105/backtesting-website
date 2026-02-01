0316a42910301fb6bbb0e94ce810634bd12f2d7c
Palak Jain <palakjain@Palaks-MacBook-Air.local>
2026-02-01 19:46:41 +0530

    Fix DuckDB R2 endpoint (strip scheme)

--- app/utils/duckdb_client.py
+++ app/utils/duckdb_client.py
@@ -8,9 +8,13 @@ def get_duckdb():
     con.execute("INSTALL httpfs;")
     con.execute("LOAD httpfs;")
 
-    # Cloudflare R2 = S3-compatible
+    # DuckDB expects hostname ONLY (no https://)
+    endpoint = os.environ["R2_ENDPOINT"] \
+        .replace("https://", "") \
+        .replace("http://", "")
+
     con.execute(f"""
-        SET s3_endpoint='{os.environ["R2_ENDPOINT"]}';
+        SET s3_endpoint='{endpoint}';
         SET s3_access_key_id='{os.environ["R2_ACCESS_KEY"]}';
         SET s3_secret_access_key='{os.environ["R2_SECRET_KEY"]}';
         SET s3_region='auto';
